1cb07ba2c1d4bb4e90f119a2a60c71750ed5a350
juaanjo12 <juanjocantero0123@gmail.com>
2023-10-10 14:21:51 +0200

    a1

---
+++ ejercicios javascript/funciones.js
@@ -0,0 +1,5 @@
+function MostrarNumeros(){
+    for(let i=1; i<15;i++){
+        document.writer(`linea ${i} <br>`)
+    }
+}
---
+++ ejercicios javascript/practica2/ejercicio1.js
@@ -0,0 +1,22 @@
+// Crear un array llamado meses con los nombres de los meses
+function meses(){
+var meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
+  
+  // Mostrar los nombres de los meses utilizando alert()
+  for (var i = 0; i < meses.length; i++) {
+    alert(meses[i]);
+  }
+}
